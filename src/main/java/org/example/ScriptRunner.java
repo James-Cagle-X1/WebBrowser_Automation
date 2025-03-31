b33f694b89c7574d702c3f759856e08bb22f86ca
James <jamesc.cagle@gmail.com>
2025-03-31 12:41:33 -0400

    0001: Removing unused imports

--- src/main/java/org/example/ScriptRunner.java
+++ src/main/java/org/example/ScriptRunner.java
@@ -1,9 +1,5 @@
 package org.example;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 import static org.example.Actions.*;
 
 public class ScriptRunner {
